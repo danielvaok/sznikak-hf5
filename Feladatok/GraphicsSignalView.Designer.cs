5de25797f488d79276556d6cf78f58dc323103eb
danielvaok <49104671+danielvaok@users.noreply.github.com>
2022-05-15 14:25:29 +0200

    Add files via upload

---
+++ Feladatok/GraphicsSignalView.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Signals
+{
+    partial class GraphicsSignalView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_ZoomIn = new System.Windows.Forms.Button();
+            this.b_ZoomOut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b_ZoomIn
+            // 
+            this.b_ZoomIn.Location = new System.Drawing.Point(15, 15);
+            this.b_ZoomIn.Name = "b_ZoomIn";
+            this.b_ZoomIn.Size = new System.Drawing.Size(40, 40);
+            this.b_ZoomIn.TabIndex = 0;
+            this.b_ZoomIn.Text = "+";
+            this.b_ZoomIn.UseVisualStyleBackColor = true;
+            // 
+            // b_ZoomOut
+            // 
+            this.b_ZoomOut.Location = new System.Drawing.Point(61, 15);
+            this.b_ZoomOut.Name = "b_ZoomOut";
+            this.b_ZoomOut.Size = new System.Drawing.Size(40, 40);
+            this.b_ZoomOut.TabIndex = 1;
+            this.b_ZoomOut.Text = "-";
+            this.b_ZoomOut.UseVisualStyleBackColor = true;
+            // 
+            // GraphicsSignalView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.b_ZoomIn);
+            this.Controls.Add(this.b_ZoomOut);
+            this.Name = "GraphicsSignalView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button b_ZoomIn;
+        private Button b_ZoomOut;
+    }
+}
---
+++ Feladatok/GraphicsSignalView.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Signals
+{
+    /// <summary>
+    /// Osztály, mely felel a grafikon kirajzolásáért.
+    /// </summary>
+    public partial class GraphicsSignalView : UserControl, IView
+    {
+        /// <summary>
+        /// A megfelelő kirajzolásért felelős osztályok.
+        /// </summary>
+        double pixelPerSec = 1, pixelPerValue = 100, zoom = 1;
+        /// <summary>
+        /// IView-ból implementálva.
+        /// A dokumentumhoz tartozó nézet sorszáma.
+        /// </summary>
+        public int ViewNumber
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Alapértelmezett konstruktor.
+        /// </summary>
+        public GraphicsSignalView()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// A jeleket tartalmazó dokumentum.
+        /// </summary>
+        private SignalDocument signalDocument;
+        /// <summary>
+        /// Dokumentumot átadó konstruktor.
+        /// </summary>
+        /// <param name="s">Az átadott dokumentum.</param>
+        public GraphicsSignalView(SignalDocument s)
+        {
+            signalDocument = s;
+        }
+        /// <summary>
+        /// A jeleket tartalmazó dokumentumot adja vissza.
+        /// </summary>
+        /// <returns>A jeleket tartalmazó dokumentum</returns>
+        public Document GetDocument()
+        {
+            return signalDocument;
+        }
+        /// <summary>
+        /// A rajzolást megvalósító függvény. A UserControl-ból származó függvény felülírása.
+        /// </summary>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Tengelyhez szükséges toll definiálása
+            Pen pen = new Pen(Color.Red, 2);
+            pen.DashStyle = DashStyle.Dot;
+            pen.EndCap = LineCap.ArrowAnchor;
+
+            //Tengelyek megrajzolása
+            int h = ClientSize.Height, w = ClientSize.Width;
+            Point p1 = new Point(0, h / 2), p2 = new Point(w, h / 2);
+            e.Graphics.DrawLine(pen, p1, p2);
+            p1 = new Point(2, h);
+            p2 = new Point(2, 0);
+            e.Graphics.DrawLine(pen, p1, p2);
+
+            //Pontok kirajzolása (itt már float típusúak a pontok paraméterei)
+            pen = new Pen(Color.SteelBlue, 1);
+            SignalValue prev = null;
+            float px = 0, py = 0, x = 0, y = h / 2;
+            foreach (SignalValue i in signalDocument.Signals)
+            {
+                y = h / 2;
+                if (prev != null)  x += (float)((i.GetTimeStamp().Ticks - prev.GetTimeStamp().Ticks)/10000000*pixelPerSec*zoom);
+                y -= (float)(i.GetValue() * pixelPerSec * pixelPerValue * zoom);
+                e.Graphics.FillRectangle(new SolidBrush(Color.SteelBlue), x, y, 3, 3);
+                if (prev != null) e.Graphics.DrawLine(pen, px, py, x, y);
+                prev = i;
+                px = x;
+                py = y;
+            }
+        }
+
+        /// <summary>
+        /// A nagyítás gomb működéséért felelős függvény.
+        /// </summary>
+        private void b_ZoomIn_Click(object sender, EventArgs e)
+        {
+            zoom += 0.1;
+            Update();
+        }
+
+        /// <summary>
+        /// A kicsinyítés gomb működéséért felelős függvény.
+        /// </summary>
+        private void b_ZoomOut_Click(object sender, EventArgs e)
+        {
+            zoom -= 0.1;
+            Update();
+        }
+
+        /// <summary>
+        /// Interfészből implementált függvény.
+        /// </summary>
+        public void Update()
+        {
+            Invalidate();
+        }
+    }
+}
